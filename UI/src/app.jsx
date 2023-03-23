844e37a4339d9642c7e4f0fb0fa425e2cf54af2f
HP2422 <113656267+HP2422@users.noreply.github.com>
2023-03-23 18:43:27 +0530

    Revert "Release 2.0.4"
    This reverts commit e44136b0eaf5d3efcc92b1db28231320ccf78403.

--- UI/src/app.jsx
+++ UI/src/app.jsx
@@ -3,29 +3,16 @@ import 'babel-polyfill';
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import { render } from "react-dom";
-import { HashRouter as Router } from 'react-router-dom';
-import Page from './Page.jsx';
+import EmployeeDirectory from "./EmployeeDirectory.jsx";
 
-// const element = (
-//     <React.StrictMode>
-//         <Router>
-//             <Page />
-//         </Router>
-//     </React.StrictMode>
-// );
-
-// ReactDOM.render(element, document.getElementById('contents'));
+// ReactDOM.render(<EmployeeDirectory />, document.getElementById('contents'));
 
 const root = ReactDOM.createRoot(document.getElementById("contents"));
 root.render(
     <React.StrictMode>
-        <Router>
-            <Page />
-        </Router>
+        <EmployeeDirectory />
     </React.StrictMode>
 );
-
 
 if (module.hot) {
     module.hot.accept();
